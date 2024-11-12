2467bb7e73e8c2065b49adca51048aa7956a7a6f
Rodwin Noreña <rodwin osorio>
2024-11-12 11:01:39 -0500

    se elimino dependencia de iconos

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon } from "@primer/octicons-react"
+
 import Link from "next/link"
 import { ActiveLink } from "../active-link/ActiveLink"
 
@@ -18,7 +18,7 @@ export const NavBar = () => {
    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
 
     <Link href={'/'} className="flex items-center">
-        <HomeIcon className="mr-2"/>
+        
         <span>Home</span>
     </Link>
 
